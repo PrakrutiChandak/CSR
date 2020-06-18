d4e0afc5428d05d6ecc9e15564197fc233c0ce01
Prakruti <prakrutichandak@gmail.com>
2020-06-19 00:11:22 +0530

    first commit

---
+++ csr/event/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Event(models.Model):
+    # event_id = models.IntegerField()
+    # event_name = models.CharField(max_length=100)
+    # event_description = models.CharField(max_length=1000)
+    # organizer_id 
+    # nof_participants
+    # criteria = 
+    # start_date_reg = 
+    # end_date_reg = 
+    # start_date_event = 
+    # end_date_event = 
+
+    # def send_invite():
+    #     pass
+
+    # def send_query():
+    #     pass
+
+    # def send_reply():
+    #     pass
+
+
+
+
+    
