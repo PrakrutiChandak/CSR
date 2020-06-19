3b0107e229242a734a645efbfe5d148433ff7949
Prakruti <prakrutichandak@gmail.com>
2020-06-19 20:04:03 +0530

    added models partially

--- csr/event/models.py
+++ csr/event/models.py
@@ -1,27 +1,22 @@
 from django.db import models
 
 class Event(models.Model):
+    __tablename__ = 'event'
     # event_id = models.IntegerField()
     # event_name = models.CharField(max_length=100)
     # event_description = models.CharField(max_length=1000)
-    # organizer_id 
-    # nof_participants
+    organizer_id = models.IntegerField()
+    # nof_participants =
     # criteria = 
-    # start_date_reg = 
-    # end_date_reg = 
-    # start_date_event = 
-    # end_date_event = 
-
-    # def send_invite():
-    #     pass
-
-    # def send_query():
-    #     pass
-
-    # def send_reply():
-    #     pass
+    start_date_reg = models.DateTimeField() 
+    end_date_reg = models.DateTimeField()
+    start_date_event = models.DateTimeField() 
+    end_date_event = models.DateTimeField()
 
 
+class User(models.Model):
+    __abstract__ = True
 
+class Employee(User):
+    __tablename__ = 'employee'
 
-    
