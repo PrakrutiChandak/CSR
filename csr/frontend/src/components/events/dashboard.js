4e15f093239990ad99cc3f555ce4f3065fb0bc0a
Prakruti <prakrutichandak@gmail.com>
2020-07-04 16:14:32 +0530

    added react

---
+++ csr/frontend/src/components/events/dashboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import form from './form';
+import events from './events';
+
+export default function dashboard() {
+    return (
+        <div>
+            <form />
+            <events />
+        </div>
+    )
+}
---
+++ csr/frontend/src/components/events/events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class events extends Component {
+    render() {
+        return (
+            <div>
+                <h1> events list</h1>
+            </div>
+        )
+    }
+}
+
+export default events
