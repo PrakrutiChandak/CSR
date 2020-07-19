a6cbd872d7cff6fa5ce0fb82af851332ad6564b7
PrakrutiChandak <prakrutichandak@gmail.com>
2020-07-19 17:21:42 +0530

    .

--- csr/csr/wsgi.py
+++ csr/csr/wsgi.py
@@ -1,16 +1,22 @@
-"""
-WSGI config for csr project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
+# This file contains the WSGI configuration required to serve up your
+# web application at http://PrakrutiChandak.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
 
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = '/home/PrakrutiChandak/csr'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'csr.settings'
+
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csr.settings')
-
 application = get_wsgi_application()
